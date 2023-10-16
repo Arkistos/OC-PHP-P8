7700e8073b0eb137c927ca972eb41fdf6a4d8a35
Arkistos <pierre.laco@hotmail.fr>
2023-10-16 12:24:28 +0300

    fix: complete database connection

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,6 +12,7 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+        echo 'homepage';
         return $this->render('default/index.html.twig');
     }
 }
